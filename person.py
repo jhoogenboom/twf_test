bb73423c508f893450a7ca18745d5657f8e03bbf
Hans <j.j.j.g.hoogenboom@tudelft.nl>
2017-11-23 15:42:58 +0100

    Added a group to hold aPerson

--- person.py
+++ person.py
@@ -4,4 +4,8 @@ class aPerson(object):
     self.__init__(self, name, age):
         self.name = name
         self.age  = age
-    
+
+
+class aGroup(object):
+    self.__init__(self, persons)
+    self.persons = persons
