ef9e4adad4018bf9084c3f682c5f94d704b752e9
Hans <j.j.j.g.hoogenboom@tudelft.nl>
2017-11-23 08:03:29 +0100

    Added a test subject

---
+++ someCode.py
@@ -0,0 +1,13 @@
+#! /bin/env python
+
+def guineaPig(name, getUp, gotoBed)
+    self.name =  name
+    self.getUp = getUp
+    self.gotoBed = gotoBed
+
+    self.awake(self.getUp, self.gotoBed):
+        return self.gotoBed - self.getUp
+
+
+testSubjects = []
+testSubject.append(guinePig("David", 7, 23))
